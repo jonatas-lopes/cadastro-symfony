f981ffd1db56487d073674943048084c51a16267
jonatas-lopes <jonatas.lopes95@live.com>
2020-08-11 17:21:09 -0300

    .

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController {
+
+    /**
+     * @Route("/", methods={"GET"})
+     */
+    public function index(Request $request)
+    {
+       $resp = new Response(json_encode([
+           "recebido" => $request->get('nome'),
+           "ip" => $request->getClientIp()
+       ]));
+       
+       $resp->setStatusCode(200);
+
+       return $resp;
+    }
+
+
+}
